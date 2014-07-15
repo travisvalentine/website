60c94bfccc172824d84a98d8e4a98eb72a299d4f
Travis Valentine <tvalent2@gmualumni.org>
2014-07-14 23:11:29 -0400

    Add a bunch of stuff for sinatra

---
+++ application.rb
@@ -0,0 +1,22 @@
+require 'rubygems'
+require 'bundler/setup'
+require 'sinatra'
+require 'sinatra/link_header'
+
+configure do
+  set :views, "#{File.dirname(__FILE__)}/views"
+end
+
+error do
+  e = request.env['sinatra.error']
+  Kernel.puts e.backtrace.join("\n")
+  'Application error'
+end
+
+helpers do
+  Sinatra::LinkHeader
+end
+
+get '/' do
+  erb :index
+end
